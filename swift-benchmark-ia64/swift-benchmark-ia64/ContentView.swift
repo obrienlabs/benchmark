e7f86fe962f9d3f056908cbc9249df1a1cce568f
Michael O'Brien <michael@obrienlabs.org>
2021-10-03 19:40:54 -0400

    #3 - adjust for not using shift right/left - m1 macmini is the fastest

--- swift-benchmark-ia64/swift-benchmark-ia64/ContentView.swift
+++ swift-benchmark-ia64/swift-benchmark-ia64/ContentView.swift
@@ -5,11 +5,14 @@
 //  Created by michaelobrien on 2021-10-03.
 //  20210930: 10 million 9232 collatz iterations - single core
 //  36.58 sec on i9-9980 MBP16 emulating iphone 13 pro
-//  24.56 sec on i9-9980 MBP16 native ia64
+//  23.33 sec on i9-9980 MBP16 2019 native ia64
+//  21.11 sec ib i9-9980 MBP15 2019 native ia64
+//  14.45 sec on M1 macmini 2020 - rosetta
 //  13.32 sec on ipad pro 2nd gen 11inch
 //  11.23 sec on iphone 12 pro max
 //  11.10 sec on iphone 12 pro
-//  10.48 sec on ipad pro 3nd gen 12.9 inch
+//  10.60 sec on M1 macmini 2020
+//  10.48 sec on M1 ipad pro 3nd gen 12.9 inch
 //
 //
 
@@ -32,11 +35,12 @@ struct ContentView: View {
         let time_start = DispatchTime.now()
         for _ in 1...10_000_000 {
         i = start;
+        path = 1;
         while i > 1 {
             if i % 2 == 0 {
-                i = i / 2
+                i = i / 2 //i >> 1 // shift takes 12 times longer than divide
             } else {
-                i += 2 * i + 1
+                i += 2 * i + 1 //i << 1 + 1 // shift takes 30% longer than multiply
             }
             if i > max {
                 max = i
