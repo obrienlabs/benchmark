b6e27befac9c6341ae18af77193731a15e35c845
Michael O'Brien <michael@obrienlabs.org>
2021-10-02 21:16:51 -0400

    #2 - add swift5 benchmark

---
+++ swift-benchmark/swift-benchmark/swift_benchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_benchmarkApp.swift
+//  swift-benchmark
+//
+//  Created by michaelobrien on 2021-10-02.
+//
+
+import SwiftUI
+
+@main
+struct swift_benchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
